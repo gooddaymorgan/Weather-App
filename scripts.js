0f737e30e5f2938e825298cbceabbbdf485ba56d
Leah Chater <LeahChater@gmail.com>
2023-01-30 17:54:56 -0600

    added for loop that console.logs some hourly data -added a for loop into the fetchWeather function that loops through the hourly infomation and console.logs the data

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
 
 async function fetchWeather(url) {
-    await fetch(url)
+    return fetch(url)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+
+            console.log(data.hourly.time.length)
+            for (let i = 0; i < data.hourly.time.length; i++) {
+                console.log(`Time: ${data.hourly.time[i]} `)
+                console.log(`Temperature: ${data.hourly.temperature_2m[i]}`)
+                console.log(`Humidity: ${data.hourly.relativehumidity_2m[i]}`)
+                console.log(`Wind direction: ${data.hourly.winddirection_10m[i]}`)
+            }
+        })
         .catch(error => console.error('Error:', error))
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
-fetchWeather('https://api.open-meteo.com/v1/forecast?latitude=50.45&longitude=-104.62&hourly=temperature_2m,relativehumidity_2m,winddirection_10m')
+    (fetchWeather('https://api.open-meteo.com/v1/forecast?latitude=50.45&longitude=-104.62&hourly=temperature_2m,relativehumidity_2m,winddirection_10m'));
 });
+
+
