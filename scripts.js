c1098f8f6ea49c3289140b4275a5ad5e9e19c5ba
Leah Chater <LeahChater@gmail.com>
2023-01-31 18:17:58 -0600

    got hourly weather data to display to the page
    - created new funtion updateHourlyWeather that adds html to the <div> "hourly-report"
    - changed console.logs in fetchHourlyWeather function to add the data to hourly_html instead.
    - added code into fetchHourlyWeather that runs updateHourlyWeather with the variable hourly_html

--- scripts.js
+++ scripts.js
@@ -4,14 +4,18 @@ async function fetchHourlyWeather(url) {
         .then(response => response.json())
         .then(data => {
             console.log(data)
-
-            console.log(data.hourly.time.length)
-            for (let i = 0; i < data.hourly.time.length; i++) {
-                console.log(`Time: ${data.hourly.time[i]}`)
-                console.log(`Temperature: ${data.hourly.temperature_2m[i]}${data.hourly_units.temperature_2m}`)
-                console.log(`Humidity: ${data.hourly.relativehumidity_2m[i]}${data.hourly_units.relativehumidity_2m}`)
-                console.log(`Wind direction: ${data.hourly.winddirection_10m[i]}${data.hourly_units.winddirection_10m}`)
+            let hourly_html = "";
+            
+            for (let i = 0; i <= 24; i++) {
+                hourly_html += `<div class="hourlyWeatherCard">`
+                hourly_html += `<p>Time: ${data.hourly.time[i]}</p>`
+                hourly_html += `<p>Temperature: ${data.hourly.temperature_2m[i]}${data.hourly_units.temperature_2m}</p>`
+                hourly_html += `<p>Humidity: ${data.hourly.relativehumidity_2m[i]}${data.hourly_units.relativehumidity_2m}</p>`
+                hourly_html += `<p>Wind direction: ${data.hourly.winddirection_10m[i]}${data.hourly_units.winddirection_10m}</p>`
+                hourly_html += `</div>`
             }
+
+            updateHourlyWeather(hourly_html);
         })
         .catch(error => console.error('Error:', error))
 }
@@ -75,6 +79,12 @@ function updateDailyWeather(html) {
 }
 
 
+function updateHourlyWeather(html) {
+    const Hourly_report = document.getElementById("Hourly-report");
+
+    Hourly_report.innerHTML += html
+}
+
 document.addEventListener("DOMContentLoaded", (event) => {
     fetchCurrentWeather('https://api.open-meteo.com/v1/forecast?latitude=50.45&longitude=-104.62&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration&current_weather=true&timezone=CST')
 
@@ -82,7 +92,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     fetchDailyWeather('https://api.open-meteo.com/v1/forecast?latitude=50.45&longitude=-104.62&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration&current_weather=true&timezone=CST')
 
 
-    //  fetchHourlyWeather('https://api.open-meteo.com/v1/forecast?latitude=50.45&longitude=-104.62&hourly=temperature_2m,relativehumidity_2m,winddirection_10m&timezone=CST');
+    fetchHourlyWeather('https://api.open-meteo.com/v1/forecast?latitude=50.45&longitude=-104.62&hourly=temperature_2m,relativehumidity_2m,winddirection_10m&timezone=CST');
 });
 
 
