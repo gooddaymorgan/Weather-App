b78a834654d4b64851192c37a78faf64c035151b
Leah Chater <LeahChater@gmail.com>
2023-02-01 19:15:01 -0600

    removed the code to run fetch functions when DOM loads
    - removed the code to run fetch functions when DOM loads so that the user has to select a city before the user sees any data

--- scripts.js
+++ scripts.js
@@ -74,16 +74,6 @@ function updateHourlyWeather(html) {
     Hourly_report.innerHTML = html
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    fetchCurrentWeather('https://api.open-meteo.com/v1/forecast?latitude=50.45&longitude=-104.62&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration&current_weather=true&timezone=CST')
-
-
-    fetchDailyWeather('https://api.open-meteo.com/v1/forecast?latitude=50.45&longitude=-104.62&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration&current_weather=true&timezone=CST')
-
-
-    fetchHourlyWeather('https://api.open-meteo.com/v1/forecast?latitude=50.45&longitude=-104.62&hourly=temperature_2m,relativehumidity_2m,winddirection_10m&timezone=CST');
-});
-
 
 function changeCityWeather() {
     let city = document.getElementById("city")
