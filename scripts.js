e809fb5861dc9b807c518130675e3e398121f5e4
Leah Chater <LeahChater@gmail.com>
2023-02-02 10:45:46 -0600

    created object for the weather codes

--- scripts.js
+++ scripts.js
@@ -1,4 +1,35 @@
 
+const weather_codes = {
+    '0':'Clear sky',
+    '1':'Mainly clear',
+    '2':'Partly cloudy',
+    '3':'Overcast',
+    '45':'Fog',
+    '48':'Depositing rime fog',
+    '51':'Drizzle: Light',
+    '53':'Drizzle: Moderate',
+    '55':'Drizzle: Dense',
+    '56':'Freezing Drizzle: Light',
+    '57':'Freezing Drizzle: Moderate',
+    '61':'Rain: Slight',
+    '63':'Rain: Moderate',
+    '65':'Rain: Heavy',
+    '66':'Freezing Rain: Light',
+    '67':'Freezing Rain: Heavy',
+    '71':'Snow fall: Slight',
+    '73':'Snow fall: Moderate',
+    '75':'Snow fall: Heavy',
+    '77':'Snow grains',
+    '80':'Rain showers: Slight',
+    '81':'Rain showers: Moderate',
+    '82':'Rain showers: Violent',
+    '85':'Snow showers: slight',
+    '86':'Snow showers Heavy',
+    '95':'Thunderstorm',
+    '96':'Thunderstorm: Slight hail',
+    '99':'Thunderstorm: Heavy hail'
+};
+
 async function fetchHourlyWeather(url) {
     return fetch(url)
         .then(response => response.json())
